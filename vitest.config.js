d6cdc53de948d79cfd9f5348b4d411b53c8a00bc
Kamil Piechaczek <pomek@users.noreply.github.com>
2026-03-19 21:23:09 +0100

    Add Vitest V8 coverage reporting.

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        include: ['src/**/*.test.js']
+        include: ['src/**/*.test.js'],
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.js'],
+            exclude: ['src/**/*.test.js'],
+            reporter: ['text', 'html']
+        }
     }
 });
